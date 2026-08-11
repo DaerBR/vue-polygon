ec5e46466881405e571af9bd7ca8aa298382e6aa
Gennadii Guiliakov <gennadii.guliakov@limestonedigital.com>
2026-08-11 13:56:34 +0300

    404 page

---
+++ app/error.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  error: NuxtError;
+}>();
+</script>
+
+<template>
+  <NuxtLayout name="default">
+    <div class="flex flex-col items-center justify-center text-center py-20">
+      <h2 class="text-paragraph-l text-dual-orange-600">Page not found, sorry</h2>
+    </div>
+  </NuxtLayout>
+</template>
